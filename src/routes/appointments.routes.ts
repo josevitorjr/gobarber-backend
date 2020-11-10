01edfe3d6e9642717f280368f4c8af7a5c431d68
josevitorjr <josevitorrjunior@outlook.com>
2020-11-10 03:31:49 -0300

    add appointment service

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO} from 'date-fns';
+import { parseISO} from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,24 +14,18 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date} = request.body;
+  try{
+    const { provider, date} = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+    const appointment = createAppointment.execute({ provider, date: parsedDate});
 
-  if (findAppointmentInSameDate){
-      return response
-          .status(400)
-          .json({message: 'This appointment is alredy booked'});
-  };
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
+    return response.json(appointment);
+  }catch(err){
+    return response.status(400).json({message: err.message});
+  }
 });
 
 export default appointmentsRouter;
